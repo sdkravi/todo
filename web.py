07952251107aa8dce60838908b7fa51421bca02a
ravi <sdkravi1@gmail.com>
2025-10-18 13:55:32 +0530

    add new todo feature

--- web.py
+++ web.py
@@ -1,11 +1,19 @@
 import streamlit as st
 from functions import readFile, writeFile
 
+todos = readFile()
+
+def add_todo():
+    todo = st.session_state["todo_input"]
+    todos.append(todo)
+    writeFile(todos)
+
 st.title("A to-do App")
 st.subheader("This is a to-do list")
 st.write("please suggest!.")
 
-for todo in readFile():
+for todo in todos:
     st.checkbox(todo)
 
-st.text_input("", placeholder="Enter a to-do item...")
+st.text_input("", placeholder="Enter a to-do item...",key="todo_input", on_change=add_todo)
+
